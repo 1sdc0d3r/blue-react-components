f6077285f94e07f64559391f2d83c9a23281f4b8
Braden <fish0859@gmail.com>
2020-08-31 17:25:53 -0600

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
-import { ExampleComponent } from 'blue-react-components'
+import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  ExampleComponent,
+  TestComponent,
+  Navigation
+} from 'blue-react-components'
 import 'blue-react-components/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <Router>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <TestComponent text='testing123' />
+      <Navigation
+        data={{
+          Home: 'ToHome',
+          Team: 'ToTeam',
+          Work: 'ToWork',
+          Blog: 'ToBlog',
+          Contact: 'ToContact'
+        }}
+      />
+    </Router>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+import { NavLink } from 'react-router-dom'
+
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className={styles.example}>Example Component: {text}</div>
+}
+
+export const TestComponent = ({ text, color }) => {
+  return (
+    <div className={styles.test} style={{ color: color }}>
+      Test: {text}
+    </div>
+  )
+}
+
+export const Navigation = (props) => {
+  console.log(props)
+  const { data } = props
+  return (
+    <nav>
+      {Object.entries(data).map((e) => (
+        <NavLink to={e[1]} className='nav-item' key={e}>
+          {e[0]}
+        </NavLink>
+      ))}
+    </nav>
+  )
 }
